0b5504f9dc38a30002f27bae2966de96c44a0bd7
Wade Barnes <wade.barnes@shaw.ca>
2019-04-26 13:44:19 -0700

    Add authentication to backend screens.

--- src/iiw_book_service/urls.py
+++ src/iiw_book_service/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("iiw_book.urls")),
     path("admin/", admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
