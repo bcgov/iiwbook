ca62f9568bfeee57e81a6f57839c01a40435665e
Nicholas Rempel <nbrempel@gmail.com>
2019-10-20 15:03:20 -0700

    quick and dirty polling  approach for  iiwbook  state updates
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- src/iiw_book/urls.py
+++ src/iiw_book/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("get-invite", views.invite, name="invite"),
+    path("state/<str:connection_id>", views.state, name="state"),
+    path(
+        "in-progress/<str:connection_id>",
+        views.in_progress,
+        name="in_progress",
+    ),
     path("backend", views.backend, name="backend"),
     path("backend/denied", views.backend_denied, name="backend_denied"),
     path("backend/approved", views.backend_approved, name="backend_approved"),
