478950719e6bc9be25c7b3901ce74f21574dcb57
Nicholas Rempel <nbrempel@gmail.com>
2019-11-30 19:16:50 -0800

    Change restricted did
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- src/iiw_book/views.py
+++ src/iiw_book/views.py
@@ -221,7 +221,7 @@ def webhooks(request, topic):
                                 "schema_name": "verified-email",
                             },
                             {
-                                "issuer_did": "85459GxjNySJ8HwTTQ4vq7",
+                                "issuer_did": "V4nB6Q4fQEbiD1W3B39xgy",
                                 "schema_name": "verified_person",
                             },
                         ],
