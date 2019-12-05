1bd8f62eb306b38a7898c3da278fef0c81b03339
Nicholas Rempel <nbrempel@gmail.com>
2019-12-05 12:49:48 -0800

    turn back on protected backend
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- src/iiw_book/views.py
+++ src/iiw_book/views.py
@@ -104,21 +104,21 @@ def in_progress(request, connection_id):
     )
 
 
-# @login_required
+@login_required
 def backend(request):
     template = loader.get_template("backend.html")
     attendees = Attendee.objects.filter(approved=False, denied=False)
     return HttpResponse(template.render({"attendees": attendees}, request))
 
 
-# @login_required
+@login_required
 def backend_denied(request):
     template = loader.get_template("backend_denied.html")
     attendees = Attendee.objects.filter(approved=False, denied=True)
     return HttpResponse(template.render({"attendees": attendees}, request))
 
 
-# @login_required
+@login_required
 def backend_approved(request):
     template = loader.get_template("backend_approved.html")
     attendees = Attendee.objects.filter(approved=True)
