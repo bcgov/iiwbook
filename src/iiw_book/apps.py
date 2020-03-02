b0cfce2e85a67e12d3ebbbeac8fdf9ebc7b6bc9a
Nicholas Rempel <nbrempel@gmail.com>
2020-03-02 14:45:37 -0800

    Update flow to request name and conference
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- src/iiw_book/apps.py
+++ src/iiw_book/apps.py
@@ -25,7 +25,7 @@ class IIWBookConfig(AppConfig):
         if cache.get("credential_definition_id") is None:
             schema_body = {
                 "schema_name": "iiw_attendance",
-                "schema_version": "1.2.4",
+                "schema_version": "9000.0.10",
                 "attributes": ["email", "full_name", "time"],
             }
             schema_response = requests.post(f"{AGENT_URL}/schemas", json=schema_body)
--- src/iiw_book/urls.py
+++ src/iiw_book/urls.py
@@ -6,11 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("get-invite", views.invite, name="invite"),
     path("state/<str:connection_id>", views.state, name="state"),
-    path(
-        "in-progress/<str:connection_id>",
-        views.in_progress,
-        name="in_progress",
-    ),
+    path("submit-name/<str:connection_id>", views.submit_name, name="submit_name"),
+    path("in-progress/<str:connection_id>", views.in_progress, name="in_progress"),
     path("backend", views.backend, name="backend"),
     path("backend/denied", views.backend_denied, name="backend_denied"),
     path("backend/approved", views.backend_approved, name="backend_approved"),
