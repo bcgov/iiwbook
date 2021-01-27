951c2d274b4f510ebbe01abc27032c176cf829d7
wadeking98 <wkingnumber2@gmail.com>
2021-01-26 16:13:02 -0800

    working configurations

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,13 +1,4 @@
-export PROJECT_NAMESPACE="devex-von-image"
-export GIT_URI="https://github.com/bcgov/iiwbook.git"
+export PROJECT_NAMESPACE="a99fd4"
+export GIT_URI="https://github.com/wadeking98/iiwbook.git"
 export GIT_REF="master"
 
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Override environments, since there is only one AT THE MOMENT:
-# devex-von-image-tools
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-export TOOLS="devex-von-image-tools"
-export DEPLOYMENT_ENV_NAME="dev"
-export DEV="dev"
-export TEST="test"
-export PROD="prod"
