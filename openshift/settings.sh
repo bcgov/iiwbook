324b0ddc70e53eaf031b60719b1aac14c42e7b61
wadeking98 <wkingnumber2@gmail.com>
2021-01-27 10:58:10 -0800

    recreate pull request

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,13 +1,4 @@
-export PROJECT_NAMESPACE="devex-von-image"
+export PROJECT_NAMESPACE="a99fd4"
 export GIT_URI="https://github.com/bcgov/iiwbook.git"
 export GIT_REF="master"
 
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Override environments, since there is only one AT THE MOMENT:
-# devex-von-image-tools
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-export TOOLS="devex-von-image-tools"
-export DEPLOYMENT_ENV_NAME="tools"
-export DEV="tools"
-export TEST="tools"
-export PROD="tools"
